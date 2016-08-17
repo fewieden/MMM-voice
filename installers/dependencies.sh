f268332179f4a301c46e5803dba93cb5f5f64c12
fewieden <fewieden@htwg-konstanz.de>
2016-08-17 18:42:03 +0200

    replaced wget with git clone

--- installers/dependencies.sh
+++ installers/dependencies.sh
@@ -24,85 +24,130 @@ echo '     ......NMy.....`'
 echo '     mMMMMMMMMMMMMMo'
 echo -e "\e[0m"
 
-if sudo apt-get install bison libasound2-dev autoconf automake libtool python-dev swig python-pip -y; then
-    echo -e "\e[32mInstalling Dependencies |  Done\e[0m"
+
+# installing packages
+if sudo apt-get install bison libasound2-dev autoconf automake libtool python-dev swig python-pip -y ;
+then
+    echo -e "\e[32m[STEP 1/7] Installing Packages |  Done\e[0m"
 else
-	echo -e "\e[31mInstalling Dependencies | Failed\e[0m"
+	echo -e "\e[31m[STEP 1/7] Installing Packages | Failed\e[0m"
 	exit;
 fi
 
+
+# installing sphinxbase
 cd ~
-if [ -d "$HOME/sphinxbase" ] || [ -d "$HOME/sphinxbase-master" ] ; then
-	echo -e "\e[33mIt seems like sphinxbase is already installed."
-	echo -e "To prevent overwriting, the installer will be aborted."
-	echo -e "Please rename the \e[1m~/sphinxbase \e[33mor \e[1m~/sphinxbase-master\e[33m folder and try again.\e[0m"
-	exit;
+if [ ! -d "$HOME/sphinxbase" ] ;
+then
+    if git clone https://github.com/cmusphinx/sphinxbase.git ;
+    then
+        cd sphinxbase
+    else
+        echo -e "\e[31m[STEP 2/7] Installing sphinxbase | Failed\e[0m"
+        exit;
+    fi
+else
+    cd sphinxbase
+    if ! git pull ;
+    then
+        echo -e "\e[31m[STEP 2/7] Installing sphinxbase | Failed\e[0m"
+        exit;
+    fi
 fi
-wget https://github.com/cmusphinx/sphinxbase/archive/master.zip
-tar -xvzf sphinxbase-master.zip
-mv sphinxbase-master sphinxbase
-cd sphinxbase
+
 ./autogen.sh
 ./configure --enable-fixed
 make
 sudo make install
+echo -e "\e[32m[STEP 2/7] Installing sphinxbase |  Done\e[0m"
 
+
+# installing pocketsphinx
 cd ~
-if [ -d "$HOME/pocketsphinx" ] || [ -d "$HOME/pocketsphinx-master" ] ; then
-	echo -e "\e[33mIt seems like pocketsphinx is already installed."
-	echo -e "To prevent overwriting, the installer will be aborted."
-	echo -e "Please rename the \e[1m~/pocketsphinx \e[33mor \e[1m~/pocketsphinx-master\e[33m folder and try again.\e[0m"
-	exit;
+if [ ! -d "$HOME/pocketsphinx" ] ;
+then
+    if git clone https://github.com/cmusphinx/pocketsphinx.git ;
+    then
+        cd pocketsphinx
+    else
+        echo -e "\e[31m[STEP 3/7] Installing pocketsphinx | Failed\e[0m"
+        exit;
+    fi
+else
+    cd pocketsphinx
+    if ! git pull ;
+    then
+        echo -e "\e[31m[STEP 3/7] Installing pocketsphinx | Failed\e[0m"
+        exit;
+    fi
 fi
-wget https://github.com/cmusphinx/pocketsphinx/archive/master.zip
-tar -xvzf pocketsphinx-master.zip
-mv pocketsphinx-master pocketsphinx
-cd pocketsphinx
+
 ./autogen.sh
 ./configure
 make
 sudo make install
+echo -e "\e[32m[STEP 3/7] Installing pocketsphinx |  Done\e[0m"
 
+
+# exporting paths
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
 echo "export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig" >> ~/.bashrc
+echo -e "\e[32m[STEP 4/7] Exporting paths |  Done\e[0m"
 
+
+# installing cmuclmtk-0.7
 cd ~
-if [ -d "$HOME/cmucltk" ] || [ -d "$HOME/cmuclmtk-0.7" ] ; then
-	echo -e "\e[33mIt seems like cmucltk is already installed."
-	echo -e "To prevent overwriting, the installer will be aborted."
-	echo -e "Please rename the \e[1m~/cmuclmtk \e[33mor \e[1m~/cmuclmtk-0.7\e[33m folder and try again.\e[0m"
+if [ -d "$HOME/cmuclmtk-0.7" ] ;
+then
+	echo -e "\e[31m[STEP 5/7] Installing cmuclmtk-0.7 | Failed\e[0m"
 	exit;
 fi
+
 wget https://sourceforge.net/projects/cmusphinx/files/cmuclmtk/0.7/cmuclmtk-0.7.tar.gz
 tar -xvzf cmuclmtk-0.7.tar.gz
-mv cmuclmtk-0.7 cmuclmtk
-cd cmuclmtk
+cd cmuclmtk-0.7
 ./configure
 make
 sudo make install
+cd ..
+rm -f cmuclmtk-0.7.tar.gz
+echo -e "\e[32m[STEP 5/7] Installing cmuclmtk-0.7 |  Done\e[0m"
 
+
+# installing tensorflow
 cd ~
-if [ -d "$HOME/tensorflow" ] ; then
-	echo -e "\e[33mIt seems like tensorflow is already installed."
-	echo -e "To prevent overwriting, the installer will be aborted."
-	echo -e "Please rename the \e[1m~/tensorflow\e[33m folder and try again.\e[0m"
+if [ -d "$HOME/tensorflow" ] ;
+then
+	echo -e "\e[31m[STEP 6/7] Installing tensorflow | Failed\e[0m"
 	exit;
 fi
+
 mkdir tensorflow
 cd tensorflow
 wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/raw/master/bin/tensorflow-0.9.0-cp27-none-linux_armv7l.whl
 sudo pip install tensorflow-0.9.0-cp27-none-linux_armv7l.whl
+echo -e "\e[32m[STEP 6/7] Installing tensorflow |  Done\e[0m"
 
 
+# installing g2p-seq2seq
 cd ~
-if [ -d "$HOME/g2p-seq2seq" ] || [ -d "$HOME/g2p-seq2seq-master" ] ; then
-	echo -e "\e[33mIt seems like g2p-seq2seq is already installed."
-	echo -e "To prevent overwriting, the installer will be aborted."
-	echo -e "Please rename the \e[1m~/g2p-seq2seq \e[33mor \e[1m~/g2p-seq2seq-master\e[33m folder and try again.\e[0m"
-	exit;
+if [ ! -d "$HOME/g2p-seq2seq" ] ;
+then
+    if git clone https://github.com/cmusphinx/g2p-seq2seq.git ;
+    then
+        cd g2p-seq2seq
+    else
+        echo -e "\e[31m[STEP 7/7] Installing g2p-seq2seq | Failed\e[0m"
+        exit;
+    fi
+else
+    cd g2p-seq2seq
+    if ! git pull ;
+    then
+        echo -e "\e[31m[STEP 7/7] Installing g2p-seq2seq | Failed\e[0m"
+        exit;
+    fi
 fi
-wget https://github.com/cmusphinx/g2p-seq2seq/archive/master.zip
-tar -xvzf g2p-seq2seq-master.zip
-mv g2p-seq2seq-master g2p-seq2seq
-cd g2p-seq2seq
+
 sudo python setup.py install
+echo -e "\e[32m[STEP 7/7] Installing g2p-seq2seq |  Done\e[0m"
