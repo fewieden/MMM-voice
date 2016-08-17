41827ed163539c7ef10d6b2f2ae881f887e63546
fewieden <fewieden@htwg-konstanz.de>
2016-08-17 18:59:39 +0200

    added npm dependencies

--- installers/dependencies.sh
+++ installers/dependencies.sh
@@ -28,9 +28,9 @@ echo -e "\e[0m"
 # installing packages
 if sudo apt-get install bison libasound2-dev autoconf automake libtool python-dev swig python-pip -y ;
 then
-    echo -e "\e[32m[STEP 1/7] Installing Packages |  Done\e[0m"
+    echo -e "\e[32m[STEP 1/8] Installing Packages |  Done\e[0m"
 else
-	echo -e "\e[31m[STEP 1/7] Installing Packages | Failed\e[0m"
+	echo -e "\e[31m[STEP 1/8] Installing Packages | Failed\e[0m"
 	exit;
 fi
 
@@ -43,14 +43,14 @@ then
     then
         cd sphinxbase
     else
-        echo -e "\e[31m[STEP 2/7] Installing sphinxbase | Failed\e[0m"
+        echo -e "\e[31m[STEP 2/8] Installing sphinxbase | Failed\e[0m"
         exit;
     fi
 else
     cd sphinxbase
     if ! git pull ;
     then
-        echo -e "\e[31m[STEP 2/7] Installing sphinxbase | Failed\e[0m"
+        echo -e "\e[31m[STEP 2/8] Installing sphinxbase | Failed\e[0m"
         exit;
     fi
 fi
@@ -59,7 +59,7 @@ fi
 ./configure --enable-fixed
 make
 sudo make install
-echo -e "\e[32m[STEP 2/7] Installing sphinxbase |  Done\e[0m"
+echo -e "\e[32m[STEP 2/8] Installing sphinxbase |  Done\e[0m"
 
 
 # installing pocketsphinx
@@ -70,14 +70,14 @@ then
     then
         cd pocketsphinx
     else
-        echo -e "\e[31m[STEP 3/7] Installing pocketsphinx | Failed\e[0m"
+        echo -e "\e[31m[STEP 3/8] Installing pocketsphinx | Failed\e[0m"
         exit;
     fi
 else
     cd pocketsphinx
     if ! git pull ;
     then
-        echo -e "\e[31m[STEP 3/7] Installing pocketsphinx | Failed\e[0m"
+        echo -e "\e[31m[STEP 3/8] Installing pocketsphinx | Failed\e[0m"
         exit;
     fi
 fi
@@ -86,20 +86,20 @@ fi
 ./configure
 make
 sudo make install
-echo -e "\e[32m[STEP 3/7] Installing pocketsphinx |  Done\e[0m"
+echo -e "\e[32m[STEP 3/8] Installing pocketsphinx |  Done\e[0m"
 
 
 # exporting paths
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
 echo "export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig" >> ~/.bashrc
-echo -e "\e[32m[STEP 4/7] Exporting paths |  Done\e[0m"
+echo -e "\e[32m[STEP 4/8] Exporting paths |  Done\e[0m"
 
 
 # installing cmuclmtk-0.7
 cd ~
 if [ -d "$HOME/cmuclmtk-0.7" ] ;
 then
-	echo -e "\e[31m[STEP 5/7] Installing cmuclmtk-0.7 | Failed\e[0m"
+	echo -e "\e[31m[STEP 5/8] Installing cmuclmtk-0.7 | Failed\e[0m"
 	exit;
 fi
 
@@ -111,14 +111,14 @@ make
 sudo make install
 cd ..
 rm -f cmuclmtk-0.7.tar.gz
-echo -e "\e[32m[STEP 5/7] Installing cmuclmtk-0.7 |  Done\e[0m"
+echo -e "\e[32m[STEP 5/8] Installing cmuclmtk-0.7 |  Done\e[0m"
 
 
 # installing tensorflow
 cd ~
 if [ -d "$HOME/tensorflow" ] ;
 then
-	echo -e "\e[31m[STEP 6/7] Installing tensorflow | Failed\e[0m"
+	echo -e "\e[31m[STEP 6/8] Installing tensorflow | Failed\e[0m"
 	exit;
 fi
 
@@ -126,7 +126,7 @@ mkdir tensorflow
 cd tensorflow
 wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/raw/master/bin/tensorflow-0.9.0-cp27-none-linux_armv7l.whl
 sudo pip install tensorflow-0.9.0-cp27-none-linux_armv7l.whl
-echo -e "\e[32m[STEP 6/7] Installing tensorflow |  Done\e[0m"
+echo -e "\e[32m[STEP 6/8] Installing tensorflow |  Done\e[0m"
 
 
 # installing g2p-seq2seq
@@ -137,17 +137,27 @@ then
     then
         cd g2p-seq2seq
     else
-        echo -e "\e[31m[STEP 7/7] Installing g2p-seq2seq | Failed\e[0m"
+        echo -e "\e[31m[STEP 7/8] Installing g2p-seq2seq | Failed\e[0m"
         exit;
     fi
 else
     cd g2p-seq2seq
     if ! git pull ;
     then
-        echo -e "\e[31m[STEP 7/7] Installing g2p-seq2seq | Failed\e[0m"
+        echo -e "\e[31m[STEP 7/8] Installing g2p-seq2seq | Failed\e[0m"
         exit;
     fi
 fi
 
 sudo python setup.py install
-echo -e "\e[32m[STEP 7/7] Installing g2p-seq2seq |  Done\e[0m"
+echo -e "\e[32m[STEP 7/8] Installing g2p-seq2seq |  Done\e[0m"
+
+
+# installing npm dependencies
+cd ~/MagicMirror/modules/MMM-voice
+if npm install ;
+then
+    echo -e "\e[32m[STEP 8/8] Installing npm dependencies |  Done\e[0m"
+else
+    echo -e "\e[31m[STEP 8/8] Installing npm dependencies | Failed\e[0m"
+fi
