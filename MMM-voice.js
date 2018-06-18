9ce7e56314b3ea2fbe26b3ac4c02ac831c11b81d
Sam Detweiler <sdetweil@gmail.com>
2018-06-18 08:33:14 -0500

    fix missing close paren on .bind()

--- MMM-voice.js
+++ MMM-voice.js
@@ -242,9 +242,9 @@ Module.register('MMM-voice', {
                         // hide this module
                         module.hide(1000);
                     }
-                }
-                  .bind( {c:this} // eslint-disable-line 
-                  ) 
+                  }.bind( {c:this} ) 
+                ) ;
+                  
             }
             this.sendNotification('NOW_ASLEEP', JSON.stringify(list));
         } else if (notification === 'SLEEP_WAKE') {
@@ -256,8 +256,7 @@ Module.register('MMM-voice', {
                         // show it
                         module.show(1000);
                     }
-                }
-                .bind( {c:this} // eslint-disable-line 
+                  }.bind( {c:this} ) 
                 ) ;
                 // clear the list, if any
                 this.previouslyHidden = [];
